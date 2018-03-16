f48464051bffcee654eccafb1760dacd7e08acb8
Gtosta96 <gabrieltosta3@gmail.com>
2018-03-16 01:46:13 -0300

    inserting css is working

--- app/extension/backgrounds/shared.js
+++ app/extension/backgrounds/shared.js
@@ -3,3 +3,12 @@ chrome.runtime.onInstalled.addListener((object) => {
   if (object.reason !== 'install') return;
   chrome.runtime.openOptionsPage();
 });
+
+chrome.runtime.onConnect.addListener((port) => {
+  console.log('connected ', port);
+  chrome.storage.sync.get(['html', 'css'], (items) => {
+    chrome.tabs.insertCSS({ code: items.css });
+    // too slow :(
+    // chrome.tabs.executeScript({ code: `document.body.innerHTML="${items.html}"` });
+  });
+});
--- app/extension/content-scripts/blockedPage.js
+++ app/extension/content-scripts/blockedPage.js
@@ -1,13 +1,16 @@
 const changeHTML = () => {
   // hack to edit body immediately after available
+  // TODO: put a loader here and initialize the content through shared.js
   chrome.storage.sync.get(['html', 'css'], (items) => {
     setInterval(() => {
       const html = document.getElementsByTagName('html')[0];
       if (!html) return;
       html.innerHTML = items.html;
-      console.log(items);
     }, 100);
   });
 };
 
 changeHTML();
+chrome.runtime.connect();
+// const connection = chrome.runtime.connect();
+// connection.postMessage({ started: true });

77cf631df3ac7e5dca9b264d0ef130cccaf2ab04
Gtosta96 <gabrieltosta3@gmail.com>
2018-03-17 00:04:14 -0300

    add loading before loading users' content

--- app/extension/backgrounds/shared.js
+++ app/extension/backgrounds/shared.js
@@ -6,9 +6,9 @@ chrome.runtime.onInstalled.addListener((object) => {
 
 chrome.runtime.onConnect.addListener((port) => {
   console.log('connected ', port);
+
   chrome.storage.sync.get(['html', 'css'], (items) => {
     chrome.tabs.insertCSS({ code: items.css });
-    // too slow :(
-    // chrome.tabs.executeScript({ code: `document.body.innerHTML="${items.html}"` });
+    chrome.tabs.executeScript({ code: `document.documentElement.innerHTML="${items.html}"` }, () => port.postMessage());
   });
 });
--- app/extension/content-scripts/blockedPage.js
+++ app/extension/content-scripts/blockedPage.js
@@ -1,16 +1,16 @@
 const changeHTML = () => {
+  const icon = chrome.extension.getURL('../img/loading.svg');
   // hack to edit body immediately after available
-  // TODO: put a loader here and initialize the content through shared.js
-  chrome.storage.sync.get(['html', 'css'], (items) => {
-    setInterval(() => {
-      const html = document.getElementsByTagName('html')[0];
-      if (!html) return;
-      html.innerHTML = items.html;
-    }, 100);
-  });
+  const interval = setInterval(() => {
+    const html = document.getElementsByTagName('html')[0];
+    if (!html) return;
+    html.innerHTML = (
+      '<div class="loading-container">' +
+        `<img class="loading-img" src='${icon}'>` +
+      '</div>'
+    );
+  }, 100);
+  chrome.runtime.connect().onMessage.addListener(() => clearInterval(interval));
 };
 
 changeHTML();
-chrome.runtime.connect();
-// const connection = chrome.runtime.connect();
-// connection.postMessage({ started: true });
